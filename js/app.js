8bc52656fdd7d26d8b49b0e23f0bc3f43cfbc00c
muhammadaliyev <muhammadaliyev>
2025-08-29 12:05:00 +0500

    panda

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const elImg = document.getElementById("img");
+document.querySelectorAll(".btn").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    elImg.src = `/images/${e.target.innerText}.png`;
+  });
+});
